fb99fac83ba690f874e4a0ca5283ff54018bb0de
Jhon Garcia <metalprime57@gmail.com>
2020-11-04 10:16:49 -0500

    update Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main(Main.class.getName());
+	}
+	
+	public void settings() {
+		size(500, 500);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		fill(255);
+		ellipse(50, 50, 50, 50);
 	}
 public void move() {
 	
